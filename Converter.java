929fb0277051efd5c21b4778f5d7bd058c2d8d54
ROBLUUX <38559908+ROBLUUX@users.noreply.github.com>
2018-10-09 11:44:42 +0200

    Add files via upload
    ajout exception, fully functionnal wallah

--- Converter.java
+++ Converter.java
@@ -8,72 +8,62 @@ import java.math.BigDecimal;
 import java.io.*;
 import java.util.regex.*;
 public class Converter{
-  private Pattern pattern = Pattern.compile("^[0-9]*.?[0-9]*[a-zA-Z]*$");
-  private Matcher matcher;
   private Map <String[],BigDecimal> association = new HashMap<String[], BigDecimal>();
-
+  private Pattern p = Pattern.compile("^[0-9]*\\ [a-zA-Z]*$");
   public Converter(){
     this.init();
   }
 
   public void init(){
-    this.association.put(new String[]{"inches","inch","in"}, Values.inch);
-    this.association.put(new String[]{"foot","feet","ft"}, Values.foot);
-    this.association.put(new String[]{"yard","yards","yd"}, Values.yard);
-    this.association.put(new String[]{"thou","mil","th"}, Values.thou);
-    this.association.put(new String[]{"chain","chains","ch"}, Values.chain);
-    this.association.put(new String[]{"furlong"}, Values.furlong);
-    this.association.put(new String[]{"mile","miles"}, Values.mile);
-    this.association.put(new String[]{"league","leagues"}, Values.league);
-    this.association.put(new String[]{"fathom","fathoms", "ftm"}, Values.fathom);
-    this.association.put(new String[]{"cable","cables"}, Values.cable);
-    this.association.put(new String[]{"nauticalmile","nauticalmiles","nautical miles", "nautical mile"}, Values.nauticalmile);
-    this.association.put(new String[]{"link","links"}, Values.link);
-    this.association.put(new String[]{"inches","inch","in"}, Values.inch);
-    this.association.put(new String[]{"rod","rods"}, Values.rod);
-    this.association.put(new String[]{"perch","pole","square rod", "square rods", "square perch", "poles", "square pole", "square poles"}, Values.perch);
-    this.association.put(new String[]{"rood", "roods"}, Values.rood);
-    this.association.put(new String[]{"acre","acres"}, Values.acre);
-    this.association.put(new String[]{"floz","fl oz","fluid ounce", "fluid ounces"}, Values.floz);
-    this.association.put(new String[]{"gill","gi","teacup", "tea cup","gills", "teacups", "tea cups"}, Values.gill);
-    this.association.put(new String[]{"pint","pt", "pints"}, Values.pint);
-    this.association.put(new String[]{"quart","qt","quarts"}, Values.quart);
-    this.association.put(new String[]{"gallon","gal", "gallons"}, Values.gallon);
-    this.association.put(new String[]{"grain","gr","grains"}, Values.grain);
-    this.association.put(new String[]{"drachm","dr","dram","drams", "drachms"}, Values.drachm);
-    this.association.put(new String[]{"oz","ounce","ounces"}, Values.oz);
-    this.association.put(new String[]{"pound","lb","lbs", "pounds"}, Values.pound);
-    this.association.put(new String[]{"stone","stones","st"}, Values.stone);
-    this.association.put(new String[]{"quarter","qr","qtr","quarters"}, Values.quarter);
-    this.association.put(new String[]{"hundredweight","hundred weight","hundredweights", "hundred weights", "cwt"}, Values.hundredweight);
-    this.association.put(new String[]{"ton","tons","t"}, Values.ton);
-//    System.out.println(this.getval("lb"));
-    /*Set<Entry<String[], BigDecimal>> setAssoc = this.association.entrySet();
-    Iterator<Entry<String[], BigDecimal>> it = setAssoc.iterator();
-    while(it.hasNext()){
-      Entry<String[], BigDecimal> e = it.next();
-      System.out.println(e.getKey()[0] + "\t\t\t : \t\t" + e.getValue());
-    }*/
+    this.association.put(new String[]{"inches","inch","in","m"}, Values.inch);
+    this.association.put(new String[]{"foot","feet","ft","m"}, Values.foot);
+    this.association.put(new String[]{"yard","yards","yd","m"}, Values.yard);
+    this.association.put(new String[]{"thou","mil","th","m"}, Values.thou);
+    this.association.put(new String[]{"chain","chains","ch","m"}, Values.chain);
+    this.association.put(new String[]{"furlong","m"}, Values.furlong);
+    this.association.put(new String[]{"mile","miles","m"}, Values.mile);
+    this.association.put(new String[]{"league","leagues","m"}, Values.league);
+    this.association.put(new String[]{"fathom","fathoms", "ftm","m"}, Values.fathom);
+    this.association.put(new String[]{"cable","cables","m"}, Values.cable);
+    this.association.put(new String[]{"nauticalmile","nauticalmiles","nautical miles", "nautical mile","m"}, Values.nauticalmile);
+    this.association.put(new String[]{"link","links","m"}, Values.link);
+    this.association.put(new String[]{"rod","rods","m"}, Values.rod);
+    this.association.put(new String[]{"perch","pole","square rod", "square rods", "square perch", "poles", "square pole", "square poles","m²"}, Values.perch);
+    this.association.put(new String[]{"rood", "roods","m²"}, Values.rood);
+    this.association.put(new String[]{"acre","acres","m²"}, Values.acre);
+    this.association.put(new String[]{"floz","fl oz","fluid ounce", "fluid ounces","m³"}, Values.floz);
+    this.association.put(new String[]{"gill","gi","teacup", "tea cup","gills", "teacups", "tea cups","m³"}, Values.gill);
+    this.association.put(new String[]{"pint","pt", "pints","m³"}, Values.pint);
+    this.association.put(new String[]{"quart","qt","quarts","m³"}, Values.quart);
+    this.association.put(new String[]{"gallon","gal", "gallons","m³"}, Values.gallon);
+    this.association.put(new String[]{"grain","gr","grains", "kg"}, Values.grain);
+    this.association.put(new String[]{"drachm","dr","dram","drams", "drachms", "kg"}, Values.drachm);
+    this.association.put(new String[]{"oz","ounce","ounces", "kg"}, Values.oz);
+    this.association.put(new String[]{"pound","lb","lbs", "pounds", "kg"}, Values.pound);
+    this.association.put(new String[]{"stone","stones","st", "kg"}, Values.stone);
+    this.association.put(new String[]{"quarter","qr","qtr","quarters", "kg"}, Values.quarter);
+    this.association.put(new String[]{"hundredweight","hundred weight","hundredweights", "hundred weights", "cwt", "kg"}, Values.hundredweight);
+    this.association.put(new String[]{"ton","tons","t", "kg"}, Values.ton);
   }
 
-  public boolean reader(String valeur){
-    System.out.println(valeur);
-    this.matcher = this.pattern.matcher(valeur);
-    if(!matcher.matches()) {
-      System.out.println("ca passe pas");
-      return false;01
-    }
-    else{
-      String[] valeurs = valeur.split(".");
-      for(int i = 0; i < 2; i++){
-        System.out.println("val = " + valeurs[i]);
+  public String reader(String valeur) throws FormatSaisieException{
+      String[] valeurs = valeur.split("\\ ");
+      Matcher m = this.p.matcher(valeur);
+      if(valeurs.length != 2 || !m.matches()){
+        throw new FormatSaisieException();
       }
-      double val = Double.parseDouble(valeurs[0]);
-      String key = new String(valeurs[1]);
-      System.out.println(this.convert(val, getval(key)));
-      return true;
-    }
-
+      else{
+        double val = Double.parseDouble(valeurs[0]);
+        String key = new String(valeurs[1]);
+        BigDecimal convertion = this.convert(val, getval(key));
+        int puissance = 0;
+        while (convertion.compareTo(new BigDecimal(1000)) > 0){
+          puissance+=3;
+          convertion = convertion.divide(new BigDecimal(1000));
+        }
+        convertion = convertion.setScale(10,BigDecimal.ROUND_UP);
+        return (valeur + " = " + convertion.toString() + "x10^"+ puissance +"\t" +this.getNewUnit(key));
+      }
   }
 
   public BigDecimal convert(double val, BigDecimal ratio){
@@ -86,14 +76,31 @@ public class Converter{
     }
   }
 
-  public BigDecimal getval(String key) {
+  public String getNewUnit(String clay){
     Set<Entry<String[], BigDecimal>> setAssoc = this.association.entrySet();
-    Iterator<Ent01ry<String[], BigDecimal>> it = setAssoc.iterator();
+    Iterator<Entry<String[], BigDecimal>> it = setAssoc.iterator();
     while(it.hasNext()){
       Entry<String[], BigDecimal> entry = it.next();
-      for(int i = 0; i < entry.getKey().length; i++){
-        if (entry.getKey()[i] == key)
+      for(int i = 0; i < entry.getKey().length-1; i++){
+        if(entry.getKey()[i].equals(clay)){
+          return entry.getKey()[entry.getKey().length-1];
+        }
+      }
+    }
+    return "unit wrong wallah";
+  }
+
+  public BigDecimal getval(String clay) {
+    clay = clay.trim();
+    clay = clay.toLowerCase();
+    Set<Entry<String[], BigDecimal>> setAssoc = this.association.entrySet();
+    Iterator<Entry<String[], BigDecimal>> it = setAssoc.iterator();
+    while(it.hasNext()){
+      Entry<String[], BigDecimal> entry = it.next();
+      for(int i = 0; i < entry.getKey().length-1; i++){
+        if(entry.getKey()[i].equals(clay)){
           return entry.getValue();
+        }
       }
     }
     return new BigDecimal(0);
--- Unretard.java
+++ Unretard.java
@@ -5,6 +5,9 @@ public class Unretard extends JFrame implements ActionListener{
   private Converter cv = new Converter();
   private static JTextField nombre = new JTextField(20);
   private static int qi = 100;
+  private JPanel conv;
+  private JLabel result;
+  private JTextField resultatcv = new JTextField(50);
   public Unretard(){
     super("Unretard");
     init();
@@ -15,7 +18,8 @@ public class Unretard extends JFrame implements ActionListener{
   public void init(){
     this.setFocusable(true);
     this.requestFocus();
-    this.add(convertisseur());
+    this.conv = convertisseur();
+    this.add(conv);
   }
 
   public JPanel convertisseur(){
@@ -24,14 +28,19 @@ public class Unretard extends JFrame implements ActionListener{
     envoi.addActionListener(this);
     pano.add(Unretard.nombre);
     pano.add(envoi);
+    this.resultatcv.setFocusable(false);
+    pano.add(this.resultatcv);
     return pano;
   }
 
   public void actionPerformed(ActionEvent e)
   {
-    System.out.println("ca passe");
-    if(Unretard.nombre.getText() != null)
-      this.cv.reader(Unretard.nombre.getText());
+    if(Unretard.nombre.getText() != null){
+      try{
+        this.resultatcv.setText(this.cv.reader(Unretard.nombre.getText()));
+      }
+      catch(FormatSaisieException fse){}
+    }
     else
       System.out.println("qi = " + qi);
   }
